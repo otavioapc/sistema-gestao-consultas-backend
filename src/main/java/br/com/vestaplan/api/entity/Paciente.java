15a1749c72d0f7e6ad63ba786018daedc971bb85
otavioapc <otavioaugcampos08@gmail.com>
2026-06-08 16:42:30 -0300

    feat: contrutor, getters e setters na entidade Paciente

--- src/main/java/br/com/vestaplan/api/entity/Paciente.java
+++ src/main/java/br/com/vestaplan/api/entity/Paciente.java
@@ -35,4 +35,62 @@ public class Paciente {
     @NotBlank(message = "O telefone é obrigatório")
     private String telefone;
 
+    public Paciente(String nome, String email, String cpf, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.cpf = cpf;
+        this.telefone = telefone;
+    }
+
+    public Paciente(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public @NotBlank(message = "O nome é obrigatório") String getNome() {
+        return nome;
+    }
+
+    public void setNome(@NotBlank(message = "O nome é obrigatório") String nome) {
+        this.nome = nome;
+    }
+
+    public @NotBlank(message = "O email é obrigatório") String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NotBlank(message = "O email é obrigatório") String email) {
+        this.email = email;
+    }
+
+    public @NotBlank(message = "O CPF é obrigatório") String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(@NotBlank(message = "O CPF é obrigatório") String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public @NotBlank(message = "O telefone é obrigatório") String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(@NotBlank(message = "O telefone é obrigatório") String telefone) {
+        this.telefone = telefone;
+    }
 }
