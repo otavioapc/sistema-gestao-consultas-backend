e8c2cd5d723979bb68245f0867534ae2135114bd
otavioapc <otavioaugcampos08@gmail.com>
2026-06-08 11:19:03 -0300

    feat: initial commit

---
+++ src/main/java/br/com/vestaplan/api/VestaplanApplication.java
@@ -0,0 +1,13 @@
+package br.com.vestaplan.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VestaplanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VestaplanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/vestaplan/api/VestaplanApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vestaplan.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VestaplanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
