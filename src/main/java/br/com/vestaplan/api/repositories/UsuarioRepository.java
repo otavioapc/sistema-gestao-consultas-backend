69efa1f413bc1473b3465341f4908c688f3fcf7e
otavioapc <otavioaugcampos08@gmail.com>
2026-06-09 19:19:33 -0300

    feat: métodos abstratos existsByCpf e existsByEmail no UsuarioRepository e métodos save e getUsuarios no UsuarioService

--- src/main/java/br/com/vestaplan/api/repositories/UsuarioRepository.java
+++ src/main/java/br/com/vestaplan/api/repositories/UsuarioRepository.java
@@ -4,4 +4,9 @@ import br.com.vestaplan.api.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    boolean existsByEmail(String email);
+
+    boolean existsByCpf(String cpf);
+
 }
--- src/main/java/br/com/vestaplan/api/services/UsuarioService.java
+++ src/main/java/br/com/vestaplan/api/services/UsuarioService.java
@@ -1,8 +1,12 @@
 package br.com.vestaplan.api.services;
 
+import br.com.vestaplan.api.dtos.UsuarioDTO;
+import br.com.vestaplan.api.entity.Usuario;
 import br.com.vestaplan.api.mappers.UsuarioMapper;
 import br.com.vestaplan.api.repositories.UsuarioRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UsuarioService {
@@ -15,6 +19,24 @@ public class UsuarioService {
         this.usuarioMapper = usuarioMapper;
     }
 
+    public Usuario save(UsuarioDTO dto){
+
+        if(usuarioRepository.existsByEmail(dto.email())){
+            throw new RuntimeException("Este email já está cadastrado!");
+        }
+
+        if(usuarioRepository.existsByCpf(dto.cpf())){
+            throw  new RuntimeException("Este CPF já está cadastrado");
+        }
+
+        Usuario usuarioEntity = usuarioMapper.toEntity(dto);
+
+        return usuarioRepository.save(usuarioEntity);
+    }
+
+    public List<Usuario> getUsuarios(){
+        return usuarioRepository.findAll();
+    }
 
 
 }
