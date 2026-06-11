01a2b92e42e61f174df8965a7dc1f63724ccf3f3
otavioapc <otavioaugcampos08@gmail.com>
2026-06-10 23:49:55 -0300

    feat: criacao da EspecialidadeService com save, get (por id e nome), update e delete

--- src/main/java/br/com/vestaplan/api/repositories/EspecialidadeRepository.java
+++ src/main/java/br/com/vestaplan/api/repositories/EspecialidadeRepository.java
@@ -3,5 +3,12 @@ package br.com.vestaplan.api.repositories;
 import br.com.vestaplan.api.entity.Especialidade;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EspecialidadeRepository extends JpaRepository<Especialidade, Integer> {
+
+    boolean existsByNomeIgnoreCase(String nome);
+
+    List<Especialidade> findByNomeContainingIgnoreCase(String nome);
+
 }
---
+++ src/main/java/br/com/vestaplan/api/services/EspecialidadeService.java
@@ -0,0 +1,61 @@
+package br.com.vestaplan.api.services;
+
+import br.com.vestaplan.api.dtos.EspecialidadeDTO;
+import br.com.vestaplan.api.entity.Especialidade;
+import br.com.vestaplan.api.mappers.EspecialidadeMapper;
+import br.com.vestaplan.api.repositories.EspecialidadeRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EspecialidadeService {
+
+    private final EspecialidadeRepository especialidadeRepository;
+    private final EspecialidadeMapper especialidadeMapper;
+
+    public EspecialidadeService(EspecialidadeRepository especialidadeRepository, EspecialidadeMapper especialidadeMapper) {
+        this.especialidadeRepository = especialidadeRepository;
+        this.especialidadeMapper = especialidadeMapper;
+    }
+
+    public Especialidade save(EspecialidadeDTO dto) {
+        if (especialidadeRepository.existsByNomeIgnoreCase(dto.nome())) {
+            throw new RuntimeException("Esta especialidade já está cadastrada!");
+        }
+
+        Especialidade entity = especialidadeMapper.toEntity(dto);
+        return especialidadeRepository.save(entity);
+    }
+
+    public List<Especialidade> getEspecialidades() {
+        return especialidadeRepository.findAll();
+    }
+
+    public Especialidade findById(Integer id) {
+        return especialidadeRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Especialidade com o ID " + id + " não encontrada!"));
+    }
+
+    public List<Especialidade> findByName(String nome) {
+        return especialidadeRepository.findByNomeContainingIgnoreCase(nome);
+    }
+
+    @Transactional
+    public Especialidade update(Integer id, EspecialidadeDTO dto) {
+        Especialidade atual = especialidadeRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Especialidade com o ID " + id + " não encontrada!"));
+
+        atual.setNome(dto.nome());
+        return especialidadeRepository.save(atual);
+    }
+
+    @Transactional
+    public void delete(Integer id) {
+        if (!especialidadeRepository.existsById(id)) {
+            throw new RuntimeException("Especialidade não encontrada para exclusão!");
+        }
+        especialidadeRepository.deleteById(id);
+    }
+}
